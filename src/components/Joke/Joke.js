a67e6f552b19c6ac646aa09c51c5f5f65ff3068a
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-04 18:35:49 +0100

    Joke component add vote up and down arrows

--- src/components/Joke/Joke.js
+++ src/components/Joke/Joke.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { FaArrowUp, FaArrowDown } from "./react-icons/fa";
 
 const Joke = () => {
-  return <div className="Joke"></div>;
+  return (
+    <div className="Joke">
+      <div className="Joke-buttons">
+        <FaArrowUp />
+        <span>{props.votes}</span>
+        <FaArrowDown />
+      </div>
+      <div className="Joke-text">{props.text}</div>
+    </div>
+  );
 };
 
 export default Joke;
